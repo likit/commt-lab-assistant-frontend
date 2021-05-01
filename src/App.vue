81a56437e856a795b32fa166fef61345345ba616
Likit Preeyanon <likit.pre@mahidol.edu>
2021-05-01 15:58:03 +0700

    added nav component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
+    <my-nav></my-nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import myNav from "@/components/mynav";
 export default {
   name: 'App',
+  components: {myNav},
 }
 </script>
---
+++ src/components/mynav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="../assets/robotic.png">
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">Home</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "myNav"
+}
+</script>
+
+<style scoped>
+
+</style>
